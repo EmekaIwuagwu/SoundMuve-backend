ef057b03e7234800d145d90d8459033116d97fcf
emekaiwuagwu <iwuagwuemmy@yahoo.com>
2024-05-29 13:55:02 +0100

    my changes

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require("express");
 const logger = require("morgan");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
