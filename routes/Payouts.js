6958e1d6a87ee5fb9659d861d0a6c38046d28b2b
EmekaIwuagwu <iwuagwuemmy@yahoo.com>
2024-08-11 13:03:27 +0100

    added new endpints to check Transaction status

--- routes/Payouts.js
+++ routes/Payouts.js
@@ -777,4 +777,43 @@ router.post('/resolve-account', async (req, res) => {
     }
 });
 
+router.get('/resolveTransactionStatus', async (req, res) => {
+    const { id } = req.query; // Change to req.query to match GET request conventions
+
+    console.log('Received request query:', req.query);
+
+    if (!id) {
+        return res.status(400).json({ error: 'Transaction Id is required' });
+    }
+
+    try {
+        if (
+            !req.headers.authorization ||
+            !req.headers.authorization.startsWith("Bearer ") ||
+            !req.headers.authorization.split(" ")[1]
+        ) {
+            return res.status(422).json({ message: "Please Provide Token!" });
+        }
+
+        const response = await fetch(`https://api.flutterwave.com/v3/transactions/${id}/verify`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${process.env.SECRET_KEY}`, // Use the secret key from environment variables
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (!response.ok) {
+            return res.status(response.status).json({ error: `Error verifying transaction: ${response.statusText}` });
+        }
+
+        const data = await response.json();
+
+        res.status(200).json({ message: "Transaction Retrieved", transaction: data });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
 module.exports = router;
