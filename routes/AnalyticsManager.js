07cc02657968463af20c1667ce479348051a17e7
EmekaIwuagwu <iwuagwuemmy@yahoo.com>
2024-09-30 13:44:51 +0100

    implemented corrections for the endpoints

--- routes/AnalyticsManager.js
+++ routes/AnalyticsManager.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Song = require('../models/Song');
 const { AlbumAnalytics, SingleAnalytics, Store, Location } = require('../models/AnalyticsSchema');
 const jwt = require('jsonwebtoken');
 
