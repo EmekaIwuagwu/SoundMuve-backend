c7910eb0602be1991c7f3f751235590b0dc474bc
EmekaIwuagwu <iwuagwuemmy@yahoo.com>
2024-10-07 09:39:17 +0100

    implemented minor corrections

--- models/AnalyticsSchema.js
+++ models/AnalyticsSchema.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const albumAnalyticsSchema = new mongoose.Schema({
     email: { type: String, required: true },
     album_name: { type: String, required: true },
+    album_id: { type: String, required: true },
     album_sold: { type: Number, required: true },
     stream: {
         apple: { type: Number, required: true },
@@ -26,6 +27,7 @@ const AlbumAnalytics = mongoose.model('AlbumAnalytics', albumAnalyticsSchema);
 const singleAnalyticsSchema = new mongoose.Schema({
     email: { type: String, required: true },
     single_name: { type: String, required: true },
+    singles_id: { type: String, required: true },
     single_sold: { type: Number, required: true },
     stream: {
         apple: { type: Number, required: true },
